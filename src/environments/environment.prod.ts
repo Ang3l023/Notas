2ae02d6afe0230324ad5b573a3dead746f18b225
Angelo Rojas <50628048+Ang3l023@users.noreply.github.com>
2020-06-04 21:53:14 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: 'AIzaSyDugugXhwL-NQEi3NRYp9sPqaVqhFo8QRI',
-    authDomain: 'appnotes-321e6.firebaseapp.com',
-    databaseURL: 'https://appnotes-321e6.firebaseio.com',
-    projectId: 'appnotes-321e6',
-    storageBucket: 'appnotes-321e6.appspot.com',
-    messagingSenderId: '1081203627797',
-    appId: '1:1081203627797:web:8b50eb791edeb418367b1c',
-    measurementId: 'G-6XVQTKN4CG'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: ''
   }
 };
